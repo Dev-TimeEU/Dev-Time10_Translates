67d738b01ec382ec11b45f37ff57cb94792186db
KilioZ <trix-folloow@hotmail.com>
2023-05-04 17:02:35 +0200

    Auth.php FR/EN
    Nouveau fichier

---
+++ en/Auth.php
@@ -0,0 +1,23 @@
+<?php
+return [
+  // Auth
+  'login' => "Login",
+  'register' => "Register",
+  'Authentication' => "Authentication",
+  'login_text' => "Login to your account",
+  'login__success' => "Login successful",
+  'username' => "Username",
+  'email' => "Email",
+  'password' => "Password",
+  'confirm_password' => "Confirm password",
+  'tos_check' => "I accept the terms and policy",
+  'login_button' => "Login",
+  'forgot_password' => "Forgot password?",
+  'register_button' => "Register",
+  'register_question' => "Don't have an account?",
+  'register_text' => "Create your Dev-Time account today",
+  'send' => "Send",
+  'login__question' => "Do you already have a Dev-Time account?",
+
+];
+?>
