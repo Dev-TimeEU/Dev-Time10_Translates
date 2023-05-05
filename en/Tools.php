25249bbf958bd53437c4b4f6e0018afed898e394
KilioZ <trix-folloow@hotmail.com>
2023-05-05 18:01:08 +0200

    Update Tools.php

--- en/Tools.php
+++ en/Tools.php
@@ -2,139 +2,139 @@
 return [
   // CGU GENERATOR
   'cgu_generator' => [
-    "title" => "Générateur de Conditions Générales d'Utilisations",
-    "description" => "Créer mes Conditions Générales d'Utilisations en toute simplicité avec Dev-Time.",
+    "title" => "Terms of Use Generator",
+    "description" => "Create my Terms of Use easily with Dev-Time.",
 
     "fields" => [
       "member_space" => [
-        "question" => "Le site internet comprend-il un espace membre ?",
-        "yes" => "Oui, j'ai un espace membre",
-        "no" => "Non, j'ai aucun espace membre",
+        "question" => "Does the website include a member space?",
+        "yes" => "Yes, I have a member space",
+        "no" => "No, I don't have a member space",
       ],
-      "website_service" => "Services du site internet",
-      "publishers_content" => "Qui publie du contenu sur le site internet ?",
-      "created_cgu" => "Quelle est la date de la mise en vigueur des CGU ?",
-      "generate" => "Générer les CGU",
+      "website_service" => "Website services",
+      "publishers_content" => "Who publishes content on the website?",
+      "created_cgu" => "What is the date the TOS came into effect?",
+      "generate" => "Generate the TOS",
     ],
   ],
 
   'cgv_generator' => [
-    "title" => "Générateur de Conditions Générales de Vente",
-    "description" => "Créer mes Conditions Générales de Vente en toute simplicité avec Dev-Time.",
-    "generate" => "Générer les CGV",
+    "title" => "Terms and Conditions Generator",
+    "description" => "Create my Terms and Conditions easily with Dev-Time",
+    "generate" => "Generate Terms and Conditions",
 
     "fields" => [
       "type_activity" => [
-        "question" => "Le site internet commercialise t-il des produits ou des prestations de services ?",
-        "1" => "Des produits (cela inclut les biens digitaux)",
-        "2" => "Des prestations de services",
-        "3" => "Des produits et prestations de services",
+        "question" => "Does the website market products or services?",
+        "1" => "Products (this includes digital goods)",
+        "2" => "Services",
+        "3" => "Products and services",
       ],
-      "services" => "Quels sont les produits proposés à la vente?",
-      "pays_ventes" => "La vente concerne-t-elle des territoires en particulier (autres que la France métropolitaine) ?",
+      "services" => "What products are offered for sale?",
+      "pays_ventes" => "Does the sale concern specific territories (other than metropolitan France)?",
 
-      "created_cgv" => "Quelle est la date de la mise en vigueur des CGV ?",
-      "processus_command" => "Quelle est le processus de commande ?",
+      "created_cgv" => "What is the effective date of the GTC?",
+      "processus_command" => "What is the order process?",
 
       "payement_conditions" => [
-        "title" => "Conditions de paiements",
-        "question" => "Quels sont les modes de paiement possibles ?",
-        "cb" => "Cartes bancaires",
-        "cb_more" => "Cartes bancaires et chèques",
-        "paypal" => "Paypal et autres moyens similaire",
+        "title" => "payment_conditions",
+        "question" => "What are the possible payment methods?",
+        "cb" => "credit cards",
+        "cb_more" => "Bank cards and checks",
+        "paypal" => "Paypal and other similar means",
       ],
 
       "paiement_effectued" => [
-        "question" => "A quel moment le paiement d'une commande est-il effectué ?",
-        "1" => "Le paiement intervient dès la passation de la commande et est débité immédiatement",
-        "2" => "Le paiement intervient dès la passation de la commande mais n'est débité qu'à l'expédition de la commande",
-        "3" => "Le paiement intervient dès la passation de la commande mais n'est débité qu'à la livraison de la commande",
-        "4" => "Le paiement n'intervient qu'à la livraison de la commande",
-        "5" => "Le paiement intervient en une fois à un terme précis",
-        "6" => "Le paiement intervient selon un échéancier sans frais",
+        "question" => "When is the payment of an order made?",
+        "1" => "Payment is made when the order is placed and is debited immediately",
+        "2" => "The payment is made when the order is placed but is only debited when the order is shipped",
+        "3" => "The payment is made when the order is placed but is only debited when the order is shipped",
+        "4" => "Payment is made upon delivery of the order",
+        "5" => "The payment is made in one go at a specific time",
+        "6" => "The payment is made according to a schedule without expenses",
       ],
 
       "livraisons" => [
-        "title" => "Livraisons",
-        "question" => "Les livraisons ont-elles lieu dans d'autres zones que la France Métropolitaine ?",
-        "question2" => "Quels sont les délais de livraison ?",
-        "question3" => "Si le délai n'est pas respecté, sous quel délai (après le dépassement de délai de livraison) le client pourra annuler la commande ?",
-        "question4" => "Les livraisons sont-elles assurées par un transporteur indépendant ?",
-        "question5" => "Délai maximal pour effectuer une réclamation (à compter de la livraison)",
-        "question6" => "Mode de réclamation (courrier, mail, etc..)",
-        "question7" => "Quel est le délai maximal de remboursement, remplacement ou réparation du(des) bien(s) non conforme(s) ou défectueux conformément à la garantie légale ?",
+        "title" => "Deliveries",
+        "question" => "Do deliveries take place in other areas than Metropolitan France?",
+        "question2" => "What are the delivery times?",
+        "question3" => "If the delivery time is not respected, how long after the delivery time has been exceeded can the customer cancel the order ?",
+        "question4" => "Are the deliveries ensured by an independent carrier ?",
+        "question5" => "Maximum time to make a claim (from the delivery)",
+        "question6" => "How can I make a complaint (mail, email, etc..)",
+        "question7" => "What is the maximum period of refund, replacement or repair of the non-conforming or defective goods according to the legal guarantee?",
       ],
 
       "data" => [
         "title" => "Traitement des données personnelles",
         "finalite_donnees" => [
-          "1" => "Traitement uniquement pour la vente des produits et leur livraison (par l'entreprise sans passer par un tiers)",
-          "2" => "Traitement uniquement pour la vente des produit et transmission à un tiers pour leur livraison",
-          "question" => "Quelle est la finalité du traitement des données personnelles collectées auprès des clients ?",
+          "1" => "Processing only for the sale of products and their delivery (by the company without going through a third party)",
+          "2" => "Processing only for the sale of the products and transmission to a third party for their delivery",
+          "question" => "What is the purpose of the processing of personal data collected from customers?",
         ],
         "donnees_collectees" => [
-          "1" => "Uniquement les prénoms, noms, adresse postale, numéro de téléphone et adresse e-mail (en plus des données pour le paiement bancaire)",
-          "2" => "Les données personnelles collectées sont réservées à l'entreprise et d'autres prestataires partenaires (exemple : prestataire de de transport, prestataire de paiement, prestataire de maintenance, etc.)",
-          "question" => "Quelles données personnelles des clients sont collectées pour l'ouverture du compte et la vente des produits ?",
+          "1" => "Only first names, surnames, postal address, telephone number and e-mail address (in addition to the data for bank payment)",
+          "2" => "The personal data collected is reserved for the company and other partner providers (e.g. transport provider, payment provider, maintenance provider, etc.)",
+          "question" => "What personal data of customers are collected for account opening and product sales?",
         ],
       ],
       "litiges" => [
-        "title" => "Litiges",
-        "question1" => "Nom du service de médiation",
-        "question2" => "Adresse postale du service de médiation",
-        "question3" => "Site internet du service de médiation",
-        "question4" => "Adresse e-mail du service de médiation",
+        "title" => "Litigation",
+        "question1" => "Name of mediation service",
+        "question2" => "Mailing address of the mediation service",
+        "question3" => "Mediation Service Website",
+        "question4" => "Mediation Service Email Address",
       ],
     ],
   ],
 
   'mentions_generator' => [
-    "generate" => "Générer les mentions légales",
+    "generate" => "Generate legal notices",
   ],
 
   'privacy_generator' => [
-    "generate" => "Générer la politique de confidentialité",
+    "generate" => "Generate privacy policy",
   ],
 
   'global' => [
-    "base_informations" => "Informations de base",
+    "base_informations" => "Basic information",
     "fields" => [
-      "website" => "Adresse du site internet",
+      "website" => "website address",
 
       "editor" => [
-        "title" => "L'éditeur du site",
+        "title" => "website editor",
         "delegue" => [
-          "question" => "Qui assure l'édition du site internet ?",
-          "not_humain" => "Une personne morale",
-          "humain" => "Une personne physique",
+          "question" => "Who is editing the website?",
+          "not_humain" => "A legal person",
+          "humain" => "A natural person",
 
-          "enterprise_name" => "Nom de la société",
-          "enterprise_capital" => "Montant du capital social en euros",
-          "enterprise_rcs" => "Ville d'immatriculation au RCS",
-          "enterprise_siren" => "Numéro de SIREN",
-          "enterprise_adress" => "Adresse du siège social",
-          "enterprise_username" => "Nom et prénom du directeur de la publication",
+          "enterprise_name" => "Name of the company",
+          "enterprise_capital" => "Amount of share capital in euros",
+          "enterprise_rcs" => "City of registration",
+          "enterprise_siren" => "SIREN number",
+          "enterprise_adress" => "Address of the registered office",
+          "enterprise_username" => "First and last name of the director of the publication",
 
-          "username" => "Nom et prénom de la personne en charge de l'édition et de la publication",
-          "adress" => "Adresse de domiciliation",
-          "tel" => "Numéro de téléphone",
-          "email" => "Adresse email",
+          "username" => "Name and first name of the person in charge of editing and publishing",
+          "adress" => "Address of the address",
+          "tel" => "Telephone number",
+          "email" => "Email address",
         ],
       ],
 
       "hoster" => [
-        "title" => "L'hébergeur",
-        "name_hoster" => "Nom de l'hébergeur",
-        "adress_hoster" => "Adresse du siège sociale de l'hébergeur",
-        "tel_hoster" => "Numéro de téléphone de l'hébergeur",
+        "title" => "The host",
+        "name_hoster" => "Name of the hoster",
+        "adress_hoster" => "Address of the hoster's headquarters",
+        "tel_hoster" => "Telephone number of the host",
       ],
 
       "collect_data" => [
-        "title" => "Collecte de données",
-        "question" => "Le site internet collecte-il des données personnelles ?",
-        "yes" => "Oui",
-        "no" => "Non",
-        "email__delete_account" => "Adresse email pour supprimer ses données",
+        "title" => "Data collection",
+        "question" => "Does the website collect personal data?",
+        "yes" => "Yes",
+        "no" => "No",
+        "email__delete_account" => "Email address to delete your data",
       ],
     ],
   ],
@@ -142,77 +142,77 @@ return [
 
   'seo' => [
     "fields" => [
-      "domain" => "Nom de domaine (https://dev-time.eu)",
-      "check_seo" => "Analyser le SEO du site web",
-      "console_seo" => "Informations sur le SEO",
+      "domain" => "Domain name (https://dev-time.eu)",
+      "check_seo" => "Analyze website SEO",
+      "console_seo" => "SEO information",
     ],
     "pagespeed" => [
-      "title" => "Vérifier la rapidité de votre site internet",
+      "title" => "Check your website's speed",
       "fields" => [
-        "desktop" => "Ordinateur",
+        "desktop" => "Computer",
         "mobile" => "Mobile",
-        "desktop_mobile_question" => "Plateforme d'essai",
+        "desktop_mobile_question" => "Testing platform",
       ],
-      "test" => "Vérifier le site web",
-      "information" => "Informations sur la page",
+      "test" => "Check website",
+      "information" => "Information about the page",
     ],
   ],
 
   'colors' => [
-    "color" => "Couleur",
-    "color2" => "Couleur {0}", // Exemple: Couleur RGB
-    "convert" => "Converssion de la couleur",
-    "information_color" => "Information de la couleur",
-    "add" => "Ajouter une couleur",
+    "color" => "Color",
+    "color2" => "Color {0}", // Example: RGB Color
+    "convert" => "Color conversion",
+    "information_color" => "Color information",
+    "add" => "Add a color",
     "colors" => [
-      "unique_color" => "Couleur unique",
-      "title" => "Palette de couleurs",
-      "description" => "Avec la palette de couleur de Dev-Time, récupère tes couleurs que tu as besoin en HEX, RBG, CMYK et bien plus encore",
-      "console" => "Informations sur la couleur",
+      "unique_color" => "unique color",
+      "title" => "Color Palette",
+      "description" => "With the Dev-Time color palette, get the colors you need in HEX, RBG, CMYK and more",
+      "console" => "Color information",
 
-      "other_color_list" => "Besoin de plusieurs couleurs ? Pas de problème ! Tout est fait pour vous ci-dessous.<br>
-        Clique sur la touche [<button class='generator-button'>Espace</button>] pour générer une couleur.",
+      "other_color_list" => "Need more than one color? No problem! Everything is done for you below.<br>
+        Click on the [<button class='generator-button'>Space</button>] key to generate a color.",
     ]
   ],
 
   "graphic_design" => [
-    "choose_colors" => "Choix de couleurs",
-    "informations_design" => "Informations sur la charte graphique",
+    "choose_colors" => "choice of colors",
+    "information_design" => "graphic_design information",
 
-    "primary_color" => "Couleur principale",
-    "accent_color" => "Coulleur accent",
-    "text_color" => "Couleur textuelle",
-    "button_color" => "Couleur de bouton",
+    "primary_color" => "primary color",
+    "accent_color" => "accent color",
+    "text_color" => "Text color",
+    "button_color" => "button color",
 
-    "choose_typo" => "Choix de typographie",
-    "primary_typo" => "Typographie principale",
-    "secondary_typo" => "Typographie secondaire",
-    "style" => "Style de la typographie",
-    "select_policefont" => "Choisir une police d'écriture",
+    "choose_typo" => "typeface choice",
+    "primary_typo" => "primary typography",
+    "secondary_typo" => "Secondary typography",
+    "style" => "Style of typography",
+    "select_policefont" => "Select a font",
 
 
-    "graphic_design" => "La charte graphique",
-    "description_graphic_design" => "Voici les éléments pour ta charte graphique",
-    "your_colors" => "Vos couleurs",
-    "your_typo" => "Vos typographies",
+    "graphic_design" => "The graphic design",
+    "description_graphic_design" => "Here are the elements for your graphic design",
+    "your_colors" => "Your colors",
+    "your_typo" => "Your typography",
   ],
 
   "converter" => [
     "fontawesome" => [
-      "title" => "Convertisseur d'icône Font Awesome vers SVG",
-      "description" => "Convertissez vos icônes Font Awesome facilement avec Dev-Time.",
-      "information" => "Font Awesome détient une part de marché de 20% parmi les sites qui utilisent des scripts de polices tiers sur leur plateforme, ce qui le place en deuxième position après Google Fonts. Font Awesome publié la première fois le 21 août 2012, ce site permet d'avoir des milliers d'icônes pour rendre son site vivant.",
+      "title" => "Font Awesome to SVG icon converter",
+      "description" => "Convert your Font Awesome icons easily with Dev-Time.",
+      "information" => "Font Awesome holds a 20% market share among sites that use third-party font scripts on their platform, second only to Google Fonts. Font Awesome first published on August 21, 2012, this site allows you to have thousands of icons to make your site come alive."
 
-      "converter" => "Que voulez-vous convertir ?",
-      "icon" => "Icône Font Awesome",
-      "family" => "Famille d'îcone",
+      "converter" => "What do you want to convert?",
+      "icon" => "Font Awesome icon",
+      "family" => "Family of icons",
       "style" => "Style",
-      "convert_button" => "Convertir",
+      "convert_button" => "Convert",
     ],
     "timestamp" => [
-      "title" => "Convertisseur de date vers Timestamp",
-      "description" => "Convertissez vos dates facilement avec Dev-Time.",
-      "information" => "Le timestamp représente la quantité de secondes passés depuis le premier janvier de l'année 1970 (début de l'heure UNIX), autrement dit, le timestamp (unix) est le nombre de secondes écoulées depuis le 1er Janvier 1970 à minuit UTC. Cette valeur est employée en informatique pour constituer un instant précis grâce à un nombre. Les valeurs timestamp peuvent être utilisés avec la plupart des langages de programmation et des systèmes de gestion des bases de données.",
+      "title" => "date to timestamp converter",
+      "description" => "Convert your dates easily with Dev-Time.",
+      "information" => "The timestamp represents the amount of seconds that have passed since the first of January of the year 1970 (start of UNIX time), in other words, the timestamp (unix) is the number of seconds that have passed since January 1st 1970 at midnight UTC. This value is used in computer science to constitute a precise moment thanks to a number. Timestamp values can be used with most programming languages and database management systems",
 
 
     ],
