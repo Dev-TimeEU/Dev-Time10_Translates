bb6d2f0ef81c7fa7a646212d4af0570cfb0d1b43
KilioZ <trix-folloow@hotmail.com>
2023-05-08 18:29:01 +0200

    Non traduit

---
+++ it/Auth.php
@@ -0,0 +1,23 @@
+<?php
+return [
+  // Auth
+  'login' => "Connexion",
+  'register' => "Inscription",
+  'Authentification' => "Authentification",
+  'login_text' => "Connexion à votre compte",
+  'login__success' => "Connexion réussie",
+  'username' => "Pseudo",
+  'email' => "Email",
+  'password' => "Mot de passe",
+  'confirm_password' => "Confirmer le mot de passe",
+  'tos_check' => "J'accepte les conditions et la politique",
+  'login_button' => "Se connecter",
+  'forgot_password' => "Mot de passe oublié ?",
+  'register_button' => "S'inscrire",
+  'register_question' => "Vous n'avez pas de compte?",
+  'register_text' => "Créez votre compte Dev-Time dès aujourd'hui",
+  'send' => "Envoyer",
+  'login__question' => "Vous avez déjà un compte Dev-Time ?",
+
+];
+?>
---
+++ it/Home.php
@@ -0,0 +1,54 @@
+<?php
+return [
+    // Pages Home: About
+    'imagine' => "Imaginez un endroit...",
+    'imagine2' => "pour votre site.,pour créer votre Forum.,pour avoir des conseils.",
+    'imagine3' => "Avec Dev-Time, la création de projet devient facile !",
+    'more' => "En savoir plus",
+    'services' => [
+      'differents_services' => "Nos différents services que nous proposons",
+      'description' => "Avec Dev-Time la création de site Web devient plus simple. Nous proposons diverses API pour les différents services que nous fournissons.",
+      'forum' => [
+        "description" => "Créez votre Forum le plus simplement possible. Créez des rôles, mettez des permissions aux utilisateurs.",
+      ],
+      'seo' => [
+        "description" => "Générez vos balises SEO, mettez les sur votre site et testez votre référencement très simplement.",
+      ],
+      'legals' => [
+        "description" => "Besoin de certains documents pour votre projet, votre entreprise, association ? Avec Dev-Time la génération de ces documents sont possibles et ce gratuitement.",
+      ],
+      'colors' => [
+        "description" => "Génèrez vos chartes graphiques en quelques clics ou récupérez les informations RGB, HEX, CMYK ou autre d'une couleur en un simple clique.",
+      ],
+      'converters' => [
+        "description" => "Convertissez différentes choses facilement avec Dev-Time, vos dates en timestamp par exemple.",
+      ],
+    ],
+
+    'signup' => "Inscrivez-vous",
+    'signup_description' => "Inscrivez-vous pour tester les différents services que nous proposons !",
+    'choose_service' => "Choix du service",
+    'choose_service_description' => "Choisissez un service qui vous convient.",
+    'go_online' => "Mise en ligne",
+    'go_online_description' => "Partagez à vos amis ce que vous avez fait avec Dev-Time pour qu'ils fassent pareil !",
+
+    'why_devtime' => "Pourquoi Dev-Time ?",
+    'why_devtime2' => "Premièrement, nous sommes sur le marché depuis 2016. Nous avons donc une large connaissance dans le domaine du web, et comment faire pour aider les petits projets.</p>
+    <p>Ensuite, nous proposons divers services pour le Web. Que ce soit du développement, de l'amélioration de SEO, de l'implémentation de fonctionnalité sur votre site. Nous sommes là pour tout !</p>
+    <p class='mb-6'>Venez tester par vous-même, c'est totalement gratuit !",
+    'tester' => "Tester",
+
+    'reviews' => "Les clients parlent de nous !",
+    'truspilot_review' => "Avis TrustPilot",
+    'happy_client' => "Clients satisfaits",
+
+    'services_used' => "Services utilisés",
+    'followers_twitter' => "Abonnés Twitter",
+    'members_discord' => "Membres sur Discord",
+    'register_members' => "Membres inscrits",
+    'discord_bot' => "Serveurs Discord",
+
+    'news' => "Actualité",
+    'news2' => "Retrouvez l'actualité de Dev-Time",
+];
+?>
