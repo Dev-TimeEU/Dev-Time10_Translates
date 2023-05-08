47cc59d474708d5cc5ca5a02d5b0c2e96b887b3a
KilioZ <trix-folloow@hotmail.com>
2023-05-08 18:28:33 +0200

    Create Dashboard.php

---
+++ it/Dashboard.php
@@ -0,0 +1,51 @@
+<?php
+return [
+  // Dashboard
+  'dashboard' => "Dashboard",
+  'my_account' => "Il mio account",
+
+  'devtime_welcome' => "Benvenuti su Dev-Time",
+  'welcome_description' => "In questo spazio potete modificare i vostri dati e scaricare gli elementi ordinati.",
+  'my_solde' => "Il mio saldo",
+  'my_solde_description' => "Attualmente avete {0, number}€ sul vostro conto Dev-Time. Potete utilizzarlo per vari servizi. Se avete bisogno di un saldo superiore, potete completarlo qui sotto.",
+
+  "my_settings" => "Le mie impostazioni",
+  "settings" => [
+    "email" => "Email",
+    "newsletters" => "Newsletter",
+    "newsletters1" => "Sì, per tutti",
+    "newsletters2" => "Solo annunci",
+    "newsletters3" => "Solo aggiornamenti",
+    "newsletters4" => "Solo collaborazioni",
+    "newsletters5" => "Disattiva per tutti",
+    "new_password" => "Nuova password",
+    "confirm_new_password" => "Conferma la nuova password",
+    "update" => "Aggiorna",
+
+    "delete_data_informations" => "Eliminando i tuoi dati Dev-Time, tutti i dati saranno eliminati. I tuoi strumenti come i forum, la gestione delle tue assunzioni, le tue statistiche saranno eliminate. Una volta premuto il pulsante, non sarà più possibile recuperare i dati, saranno persi.",
+    "delete_data" => "Elimina i miei dati",
+  ],
+
+  "store" => [
+      "store" => "I miei acquisti",
+      "last_months" => "Ultimi {0} mesi",
+
+      "title" => "Titolo",
+      "code" => "Codice",
+      "price" => "Prezzo",
+      "etat" => "Stato",
+      "actions" => "Azione",
+  ],
+
+  "affiliation" => [
+    "affiliation" => "Affiliazione Dev-Time",
+    "description" => "Affiliando gli utenti su Dev-Time, riceverete <b>200DevPoints</b> e <b>50XP</b>. Il vostro link di affiliazione è il seguente: {0}",
+    "devpoints" => "I vostri DevPoints",
+    "current_devpoints" => "Attualmente avete <b>{0} DevPoints</b>.",
+
+    "user" => "Utente",
+    "etat" => "Stato del conto",
+    "reward" => "Ricompensa",
+  ]
+];
+?>
