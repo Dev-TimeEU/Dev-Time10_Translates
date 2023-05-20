0479f3f5078d9d243b4d082d09828c08a8cbb963
KilioZ <trix-folloow@hotmail.com>
2023-05-20 02:27:29 +0200

    Nouveaux fichiers
    + Member.php

---
+++ en/Member.php
@@ -0,0 +1,17 @@
+<?php
+return [
+  // Member
+  'shop' => [
+    "success_change" => "Successful change of item",
+    "success_paid_item" => "Payment of {0} successfully",
+    "no_devpoints" => "You have {0} DevPoints. You need {1} DevPoints for this item.",
+    "inedit" => "Unique item",
+  ],
+  'profile' => [
+    "biographie" => "Biography of {0}",
+    "forums" => "{0}'s forums",
+    "signin" => "Registered",
+  ]
+
+];
+?>
---
+++ fr/Member.php
@@ -0,0 +1,17 @@
+<?php
+return [
+  // Member
+  'shop' => [
+    "success_change" => "Changement d'élément avec succès",
+    "success_paid_item" => "Paiement de {0} avec succès",
+    "no_devpoints" => "Vous avez {0} DevPoints. Vous avez besoin de {1} DevPoints pour cet élément.",
+    "inedit" => "Objet unique",
+  ],
+  'profile' => [
+    "biographie" => "Biographie de {0}",
+    "forums" => "Forums de {0}",
+    "signin" => "Inscrit",
+  ]
+
+];
+?>
