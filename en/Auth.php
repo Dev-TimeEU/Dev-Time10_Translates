175d9a490a99bcc74503d8804bece5f98a4a6045
KilioZ <trix-folloow@hotmail.com>
2023-05-21 14:25:03 +0200

    Ajouts textes dans l'auth

--- en/Auth.php
+++ en/Auth.php
@@ -19,5 +19,28 @@ return [
   'send' => "Send",
   'login__question' => "Do you already have a Dev-Time account?",
 
+  'text__connected' => "You are connected",
+
+  'error__wrongCredentials' => "Oops, looks like the account information is not correct",
+  'error__notActivated' => "Oops, the Dev-Time account is not active. If you think this is an error, contact us on the Discord. If you haven't checked your email, consider checking your spam.",
+  'error__email_not_valided' => "Oops... Your email has not been validated.",
+  'error__banned_user' => "Oops... Your account has been banned by Dev-Time.",
+  'error__closed_account' => "For a security reason, your account has been closed. Your email cannot be used again. Please contact us by email contact@dev-time.eu to unblock your account.",
+  'error__undefined' => "Unknown error...",
+  'error__banned_caracters' => "You are using banned characters. Please change your username.",
+  'error__email_undefined' => "An error has occurred. #EMAIL01",
+  'error__exist_account' => 'Sorry, but this account already exists on Dev-Time',
+  'error__no_exist_account' => 'Sorry, but this account does not exist. Please check your email.',
+
+  'success_register' => "Successful registration! You can now login to your Dev-Time account!",
+  'success__password_sended' => "Your new password has been successfully sent.",
+  'success__login' => "Successful login",
+
+
+  'update_text' => "Your {0} has just been successfully changed.",
+  'update__password_not_good' => "Your account password is incorrect."
+  'password' => "password",
+  'biographie' => "biography",
+  'newsletters' => "newsletters",
 ];
 ?>
