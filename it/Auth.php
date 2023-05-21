5462c8f175dd15e73340e9698f4a5e345bfe0394
KilioZ <trix-folloow@hotmail.com>
2023-05-21 14:28:50 +0200

    Ajouts textes manquant pour l'italien

--- it/Auth.php
+++ it/Auth.php
@@ -18,6 +18,29 @@ return [
   'register_text' => "Créez votre compte Dev-Time dès aujourd'hui",
   'send' => "Envoyer",
   'login__question' => "Vous avez déjà un compte Dev-Time ?",
+  
+  'text__connected' => "Vous êtes connecté",
 
+  'error__wrongCredentials' => "Oups, il semblerait que les informations du compte ne soit pas les bons",
+  'error__notActivated' => "Oups, le compte Dev-Time n'est pas actif. Si vous pensez qu'il s'agit d'une erreur, contactez-nous sur le Discord. Si vous n'avez pas vérifiez votre email, pensez à consulter vos spams.",
+  'error__email_not_valided' => "Oups... Votre email n'a pas été validé.",
+  'error__banned_user' => "Oups... Votre compte a été banni par Dev-Time.",
+  'error__closed_account' => "Pour une raison de sécurité, votre compte a été fermé. Votre email ne peut plus être réutilisé. Contactez nous par email contact@dev-time.eu pour débloquer votre compte.",
+  'error__undefined' => "Erreur inconnue...",
+  'error__banned_caracters' => "Vous utilisez des caractères interdit. Veuillez changer votre pseudo.",
+  'error__email_undefined' => 'Une erreur est survenue. #EMAIL01',
+  'error__exist_account' => 'Désolé, mais ce compte existe déjà sur Dev-Time',
+  'error__no_exist_account' => "Désolé, mais ce compte n'existe pas. Vérifiez votre email.",
+
+  'success_register' => "Inscription avec succès ! Vous pouvez désormais vous connecter a votre compte Dev-Time !",
+  'success__password_sended' => "Votre nouveau mot de passe vient d'être envoyé avec succès.",
+  'success__login' => "Connexion réussie",
+
+
+  'update_text' => "Votre {0} vient d'être modifié avec succès.",
+  'update__password_not_good' => "Le mot de passe de votre compte est incorrecte.",
+  'password' => "mot de passe",
+  'biographie' => "biographie",
+  'newsletters' => "newsletters",
 ];
 ?>
